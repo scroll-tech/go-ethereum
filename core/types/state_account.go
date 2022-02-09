0019ffea152dfd4ebe1257dfb074265d4b0aa865
lightsing <light.tsing@gmail.com>
2022-02-09 15:13:41 +0800

    add Hash and MarshalBytes

--- core/types/state_account.go
+++ core/types/state_account.go
@@ -17,6 +17,9 @@
 package types
 
 import (
+	"encoding/binary"
+	"github.com/ethereum/go-ethereum/trie"
+	"github.com/iden3/go-iden3-crypto/poseidon"
 	"math/big"
 
 	"github.com/scroll-tech/go-ethereum/common"
@@ -29,4 +32,49 @@ type StateAccount struct {
 	Balance  *big.Int
 	Root     common.Hash // merkle root of the storage trie
 	CodeHash []byte
+}
+
+// Hash of StateAccount
+// AccountHash = Hash(
+//	Hash(nonce, balance),
+//  Hash(
+//	  Root,
+//	  Hash(codeHashFirst16, codeHashLast16)
+//  ))
+func (s StateAccount) Hash() (*big.Int, error) {
+	nonce := new(big.Int).SetUint64(s.Nonce)
+	hash1, err := poseidon.Hash([]*big.Int{nonce, s.Balance})
+	if err != nil {
+		return nil, err
+	}
+
+	codeHashFirst16 := new(big.Int).SetBytes(s.CodeHash[0:16])
+	codeHashLast16 := new(big.Int).SetBytes(s.CodeHash[16:32])
+	hash2, err := poseidon.Hash([]*big.Int{codeHashFirst16, codeHashLast16})
+
+	rootHash, err := trie.NewHashFromBytes(s.Root.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	hash3, err := poseidon.Hash([]*big.Int{rootHash.BigInt(), hash2})
+
+	hash4, err := poseidon.Hash([]*big.Int{hash1, hash3})
+	if err != nil {
+		return nil, err
+	}
+	return hash4, nil
+}
+
+// MarshalBytes
+// [0:7] Nonce uint64 little-endian
+// [32:64] Balance
+// [64:96] Root
+// [96:128] CodeHash
+func (s StateAccount) MarshalBytes() []byte {
+	bytes := make([]byte, 128)
+	binary.LittleEndian.PutUint64(bytes, s.Nonce)
+	copy(bytes[32:64], s.Balance.Bytes())
+	copy(bytes[64:96], s.Root.Bytes())
+	copy(bytes[96:128], s.CodeHash)
+	return bytes
 }

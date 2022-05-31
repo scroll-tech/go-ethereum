ff26b57c4c57ee68c79f7eaee22ee8400a4dfe68
HAOYUatHZ <haoyu@protonmail.com>
2022-05-31 17:27:45 +0800

    rename GetStateData to GetLiveStateObject

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -325,7 +325,7 @@ func (s *StateDB) GetProofByHash(addrHash common.Hash) ([][]byte, error) {
 	return proof, err
 }
 
-func (s *StateDB) GetStateData(addr common.Address) *types.StateAccount {
+func (s *StateDB) GetLiveStateObject(addr common.Address) *types.StateAccount {
 	obj, ok := s.stateObjects[addr]
 	if !ok {
 		return nil

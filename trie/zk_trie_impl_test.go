c958ea1f4098365aa6b0a09f8e6c925382528070
Ho Vei <noelwei@gmail.com>
2022-06-08 15:39:13 +0800

    goimport and mod lint

--- trie/zk_trie_impl_test.go
+++ trie/zk_trie_impl_test.go
@@ -8,10 +8,11 @@ import (
 
 	"github.com/iden3/go-iden3-crypto/constants"
 	cryptoUtils "github.com/iden3/go-iden3-crypto/utils"
-	"github.com/scroll-tech/go-ethereum/common"
-	"github.com/scroll-tech/go-ethereum/core/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/scroll-tech/go-ethereum/common"
+	"github.com/scroll-tech/go-ethereum/core/types"
 
 	"github.com/scroll-tech/go-ethereum/ethdb/memorydb"
 )

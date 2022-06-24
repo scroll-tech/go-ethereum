77fe4883dffbba96b95bae9c57a38ba403ed45ea
Ho Vei <noelwei@gmail.com>
2022-06-24 22:52:42 +0800

    update genesis, and rule out snapshot in zktrie mode

--- core/blockchain.go
+++ core/blockchain.go
@@ -236,6 +236,11 @@ func NewBlockChain(db ethdb.Database, cacheConfig *CacheConfig, chainConfig *par
 	if cacheConfig.TraceCacheLimit != 0 {
 		blockResultCache, _ = lru.New(cacheConfig.TraceCacheLimit)
 	}
+	// override snapshot setting
+	if chainConfig.Zktrie && cacheConfig.SnapshotLimit > 0 {
+		log.Warn("snapshot has been disabled by zktrie")
+		cacheConfig.SnapshotLimit = 0
+	}
 
 	bc := &BlockChain{
 		chainConfig: chainConfig,

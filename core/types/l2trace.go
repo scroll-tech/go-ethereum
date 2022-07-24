3676fcf349bd7b5aef98fab749f7753217a693bb
HAOYUatHZ <haoyu@protonmail.com>
2022-07-24 19:41:12 +0800

    comments

--- core/types/l2trace.go
+++ core/types/l2trace.go
@@ -103,11 +103,13 @@ func NewStructLogResBasic(pc uint64, op string, gas, gasCost uint64, depth int, 
 	return logRes
 }
 
-// TODO: need to update comments
 type ExtraData struct {
 	// Indicate the call succeeds or not for CALL/CREATE op
 	CallFailed bool `json:"callFailed,omitempty"`
 	// CALL | CALLCODE | DELEGATECALL | STATICCALL: [tx.to address’s code, stack.nth_last(1) address’s code]
+	// CREATE | CREATE2: [created contract’s code]
+	// CODESIZE | CODECOPY: [contract’s code]
+	// EXTCODESIZE | EXTCODECOPY: [stack.nth_last(1) address’s code]
 	CodeList []string `json:"codeList,omitempty"`
 	// SSTORE | SLOAD: [storageProof]
 	// SELFDESTRUCT: [contract address’s account, stack.nth_last(0) address’s account]

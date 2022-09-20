ca64f914b46d80d00e19b4701e26dd752fad94d6
maskpp <maskpp266@gmail.com>
2022-09-20 14:20:13 +0800

    fix comment

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -328,13 +328,13 @@ func (ec *Client) SubscribeNewHead(ctx context.Context, ch chan<- *types.Header)
 // GetBlockResultByHash returns the BlockResult given the block hash.
 func (ec *Client) GetBlockResultByHash(ctx context.Context, blockHash common.Hash) (*types.BlockResult, error) {
 	blockResult := &types.BlockResult{}
-	return blockResult, ec.c.CallContext(ctx, &blockResult, "scr_getBlockResultByNumberOrHash", blockHash)
+	return blockResult, ec.c.CallContext(ctx, &blockResult, "scroll_getBlockResultByNumberOrHash", blockHash)
 }
 
 // GetBlockResultByNumber returns the BlockResult given the block number.
 func (ec *Client) GetBlockResultByNumber(ctx context.Context, number *big.Int) (*types.BlockResult, error) {
 	blockResult := &types.BlockResult{}
-	return blockResult, ec.c.CallContext(ctx, &blockResult, "scr_getBlockResultByNumberOrHash", toBlockNumArg(number))
+	return blockResult, ec.c.CallContext(ctx, &blockResult, "scroll_getBlockResultByNumberOrHash", toBlockNumArg(number))
 }
 
 // SubscribeNewBlockResult subscribes to block execution trace when a new block is created.

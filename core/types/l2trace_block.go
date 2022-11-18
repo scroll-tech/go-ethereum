6182fd87900314eba4bf1f04295464c4315a7090
HAOYUatHZ <haoyu@protonmail.com>
2022-11-18 17:54:09 +0800

    fix: fix `TransactionData` types

--- core/types/l2trace_block.go
+++ core/types/l2trace_block.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 
 	"github.com/scroll-tech/go-ethereum/common"
+	"github.com/scroll-tech/go-ethereum/common/hexutil"
 	"github.com/scroll-tech/go-ethereum/params"
 )
 
@@ -11,6 +12,9 @@ type TransactionData struct {
 	IsCreate bool           `json:"isCreate"`
 	From     common.Address `json:"from"`
 	*Transaction
+	V *hexutil.Big `json:"v"`
+	R *hexutil.Big `json:"r"`
+	S *hexutil.Big `json:"s"`
 }
 
 // NewTraceTransaction returns a transaction that will serialize to the trace
@@ -18,9 +22,13 @@ type TransactionData struct {
 func NewTraceTransaction(tx *Transaction, blockNumber uint64, config *params.ChainConfig) *TransactionData {
 	signer := MakeSigner(config, big.NewInt(0).SetUint64(blockNumber))
 	from, _ := Sender(signer, tx)
+	v, r, s := tx.RawSignatureValues()
 	return &TransactionData{
 		From:        from,
 		IsCreate:    tx.To() == nil,
 		Transaction: tx,
+		V:           (*hexutil.Big)(v),
+		R:           (*hexutil.Big)(r),
+		S:           (*hexutil.Big)(s),
 	}
 }

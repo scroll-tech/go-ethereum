ac033f5aa3d7bd74978656e71f30dfca957c6a91
HAOYUatHZ <haoyu@protonmail.com>
2023-02-07 13:43:07 +0800

    add L1BlockContainerAddress

--- rollup/rcfg/config.go
+++ rollup/rcfg/config.go
@@ -7,7 +7,7 @@ import (
 )
 
 func init() {
-	// TODO:
+	// TODO: use conifg; allow hardforks and different networks; check on L2 consensus layer when going decentralized
 	l2GasPriceOracleAddr := common.HexToAddress("0x420000000000000000000000000000000000000F")
 	L2GasPriceOracleAddress = &l2GasPriceOracleAddr
 }
@@ -16,6 +16,9 @@ var (
 	// L2GasPriceOracleAddress is the address of the L2GasPriceOracle
 	// predeploy
 	L2GasPriceOracleAddress *common.Address
+	// L1BlockContainerAddress is the address of the L1BlockContainer
+	// predeploy
+	L1BlockContainerAddress *common.Address
 
 	Precision = new(big.Int).SetUint64(1e9)
 

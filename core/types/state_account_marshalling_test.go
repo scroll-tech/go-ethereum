6b910dc39bc35e79131a43c36bd5fe5ac9b6e9ff
Péter Garamvölgyi <peter@scroll.io>
2023-02-07 19:50:30 +0100

    add more tests

--- core/types/state_account_marshalling_test.go
+++ core/types/state_account_marshalling_test.go
@@ -84,11 +84,50 @@ func TestMarshalUnmarshalZeroAccount(t *testing.T) {
 
 func TestMarshalUnmarshalNonEmptyAccount(t *testing.T) {
 	acc := StateAccount{
-		Nonce:            math.MaxUint64,
-		Balance:          new(big.Int).Sub(constants.Q, big.NewInt(1)),
+		Nonce:            0x11111111,
+		Balance:          big.NewInt(0x33333333),
 		Root:             common.HexToHash("123456789abcdef123456789abcdef123456789abcdef123456789abcdef1234"),
 		KeccakCodeHash:   common.Hex2Bytes("1111111111111111111111111111111111111111111111111111111111111111"),
 		PoseidonCodeHash: common.Hex2Bytes("2222222222222222222222222222222222222222222222222222222222222222"),
+		CodeSize:         0x22222222,
+	}
+
+	// marshal account
+
+	bytes, flag := acc.MarshalFields()
+
+	assert.Equal(t, 5, len(bytes))
+	assert.Equal(t, uint32(8), flag)
+
+	assert.Equal(t, common.Hex2Bytes("0000000000000000000000000000000000000000222222220000000011111111"), bytes[0].Bytes())
+	assert.Equal(t, common.Hex2Bytes("0000000000000000000000000000000000000000000000000000000033333333"), bytes[1].Bytes())
+	assert.Equal(t, common.Hex2Bytes("123456789abcdef123456789abcdef123456789abcdef123456789abcdef1234"), bytes[2].Bytes())
+	assert.Equal(t, common.Hex2Bytes("1111111111111111111111111111111111111111111111111111111111111111"), bytes[3].Bytes())
+	assert.Equal(t, common.Hex2Bytes("2222222222222222222222222222222222222222222222222222222222222222"), bytes[4].Bytes())
+
+	// unmarshal account
+
+	flatBytes := []byte("")
+
+	for _, item := range bytes {
+		flatBytes = append(flatBytes, item.Bytes()...)
+	}
+
+	acc2, err := UnmarshalStateAccount(flatBytes)
+
+	assert.Nil(t, err)
+	assertAccountsEqual(t, &acc, acc2)
+}
+
+func TestMarshalUnmarshalAccountWithMaxFields(t *testing.T) {
+	maxFieldElement := new(big.Int).Sub(constants.Q, big.NewInt(1))
+
+	acc := StateAccount{
+		Nonce:            math.MaxUint64,
+		Balance:          maxFieldElement,
+		Root:             common.BigToHash(maxFieldElement),
+		KeccakCodeHash:   common.Hex2Bytes("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"),
+		PoseidonCodeHash: maxFieldElement.Bytes(),
 		CodeSize:         math.MaxUint64,
 	}
 
@@ -101,9 +140,9 @@ func TestMarshalUnmarshalNonEmptyAccount(t *testing.T) {
 
 	assert.Equal(t, common.Hex2Bytes("00000000000000000000000000000000ffffffffffffffffffffffffffffffff"), bytes[0].Bytes())
 	assert.Equal(t, common.Hex2Bytes("30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000000"), bytes[1].Bytes())
-	assert.Equal(t, common.Hex2Bytes("123456789abcdef123456789abcdef123456789abcdef123456789abcdef1234"), bytes[2].Bytes())
-	assert.Equal(t, common.Hex2Bytes("1111111111111111111111111111111111111111111111111111111111111111"), bytes[3].Bytes())
-	assert.Equal(t, common.Hex2Bytes("2222222222222222222222222222222222222222222222222222222222222222"), bytes[4].Bytes())
+	assert.Equal(t, common.Hex2Bytes("30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000000"), bytes[2].Bytes())
+	assert.Equal(t, common.Hex2Bytes("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"), bytes[3].Bytes())
+	assert.Equal(t, common.Hex2Bytes("30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000000"), bytes[4].Bytes())
 
 	// unmarshal account
 

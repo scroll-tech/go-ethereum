ad905a58858c53096dd2aa62441373f0091fcfb1
HAOYUatHZ <haoyu@protonmail.com>
2023-02-15 12:25:16 +0800

    init rollup/rcfg/config.go
    minor

---
+++ rollup/rcfg/config.go
@@ -0,0 +1,21 @@
+package rcfg
+
+import (
+	"github.com/scroll-tech/go-ethereum/common"
+)
+
+// UsingScroll is used to enable or disable functionality necessary for scroll rollup.
+var UsingScroll bool
+
+// TODO:
+// 1. use config
+// 2. allow different networks / hardforks
+// 3. vefify in consensus layer when decentralizing sequencer
+
+var (
+	// L2MessageQueueAddress is the address of the L2MessageQueue
+	// predeploy
+	// see scroll-tech/scroll/contracts/src/L2/predeploys/L2MessageQueue.sol
+	L2MessageQueueAddress = common.HexToAddress("0x5300000000000000000000000000000000000002")
+	WithdraTrieRootSlot   = common.BigToHash(big.NewInt(0))
+)

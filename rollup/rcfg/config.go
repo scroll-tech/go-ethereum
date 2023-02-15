f3a9cebab00f2ea016696311d4429e9e64759fd1
HAOYUatHZ <haoyu@protonmail.com>
2023-02-15 13:09:40 +0800

    fix `L2MessageQueueAddress`

--- rollup/rcfg/config.go
+++ rollup/rcfg/config.go
@@ -18,6 +18,6 @@ var (
 	// L2MessageQueueAddress is the address of the L2MessageQueue
 	// predeploy
 	// see scroll-tech/scroll/contracts/src/L2/predeploys/L2MessageQueue.sol
-	L2MessageQueueAddress = common.HexToAddress("0x5300000000000000000000000000000000000002")
+	L2MessageQueueAddress = common.HexToAddress("0x5300000000000000000000000000000000000000")
 	WithdrawTrieRootSlot  = common.BigToHash(big.NewInt(0))
 )

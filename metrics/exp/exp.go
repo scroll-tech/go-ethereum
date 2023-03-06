a1b911c7cda9033d073b25d82cc139a52cd36c17
colinlyguo <colinlyguo@gmail.com>
2023-03-06 11:08:49 +0800

    feat: change prometheus metrics url

--- metrics/exp/exp.go
+++ metrics/exp/exp.go
@@ -44,7 +44,7 @@ func Exp(r metrics.Registry) {
 	// http.HandleFunc("/debug/vars", e.expHandler)
 	// haven't found an elegant way, so just use a different endpoint
 	http.Handle("/debug/metrics", h)
-	http.Handle("/debug/metrics/prometheus", prometheus.Handler(r))
+	http.Handle("/metrics", prometheus.Handler(r))
 }
 
 // ExpHandler will return an expvar powered metrics handler.
@@ -58,7 +58,7 @@ func ExpHandler(r metrics.Registry) http.Handler {
 func Setup(address string) {
 	m := http.NewServeMux()
 	m.Handle("/debug/metrics", ExpHandler(metrics.DefaultRegistry))
-	m.Handle("/debug/metrics/prometheus", prometheus.Handler(metrics.DefaultRegistry))
+	m.Handle("/metrics", prometheus.Handler(metrics.DefaultRegistry))
 	log.Info("Starting metrics server", "addr", fmt.Sprintf("http://%s/debug/metrics", address))
 	go func() {
 		if err := http.ListenAndServe(address, m); err != nil {

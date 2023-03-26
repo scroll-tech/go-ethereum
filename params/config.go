bf2dbebc42daf5110fbcf9e091d77aae1cda5583
Nazarii Denha <dengaaa2002@gmail.com>
2023-03-26 19:19:25 +0200

    fix lint

--- params/config.go
+++ params/config.go
@@ -381,7 +381,7 @@ type ChainConfig struct {
 	L1Config *L1Config `json:"l1Config,omitempty"`
 }
 
-// L1Config is l1 parameteres needed to include l1 messages in Sequencer
+// L1Config is l1 parameters needed to include l1 messages in Sequencer
 type L1Config struct {
 	L1ChainId             uint64          `json:"l1ChainId,omitempty"`
 	L1MessageQueueAddress *common.Address `json:"l1MessageQueueAddress,omitempty"`

54bbb5cec809cc48d267bf16cd0bbfea3d9159db
Max Wolff <maxcwolff@gmail.com>
2023-04-10 01:24:57 -0700

    update nonce, add hash test (fails), marshal test

--- core/types/l1_message_tx.go
+++ core/types/l1_message_tx.go
@@ -33,7 +33,7 @@ func (tx *L1MessageTx) copy() TxData {
 
 // accessors for innerTx.
 func (tx *L1MessageTx) txType() byte           { return L1MessageTxType }
-func (tx *L1MessageTx) chainID() *big.Int      { return common.Big0 }
+func (tx *L1MessageTx) chainID() *big.Int      { return common.Big0 } // todo: update
 func (tx *L1MessageTx) accessList() AccessList { return nil }
 func (tx *L1MessageTx) data() []byte           { return tx.Data }
 func (tx *L1MessageTx) gas() uint64            { return tx.Gas }
@@ -41,7 +41,7 @@ func (tx *L1MessageTx) gasFeeCap() *big.Int    { return new(big.Int) }
 func (tx *L1MessageTx) gasTipCap() *big.Int    { return new(big.Int) }
 func (tx *L1MessageTx) gasPrice() *big.Int     { return new(big.Int) }
 func (tx *L1MessageTx) value() *big.Int        { return tx.Value }
-func (tx *L1MessageTx) nonce() uint64          { return 0 }
+func (tx *L1MessageTx) nonce() uint64          { return tx.Nonce }
 func (tx *L1MessageTx) to() *common.Address    { return tx.To }
 
 func (tx *L1MessageTx) rawSignatureValues() (v, r, s *big.Int) {
@@ -49,5 +49,5 @@ func (tx *L1MessageTx) rawSignatureValues() (v, r, s *big.Int) {
 }
 
 func (tx *L1MessageTx) setSignatureValues(chainID, v, r, s *big.Int) {
-	// this is a noop for deposit transactions
+	// this is a noop for l1 message transactions
 }

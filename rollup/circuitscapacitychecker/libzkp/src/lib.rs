0083ab5e6783a14479ae4701bc8c3f5a21ad738f
HAOYUatHZ <haoyu@protonmail.com>
2023-05-11 11:21:47 +0800

    fill rust 2

--- rollup/circuitscapacitychecker/libzkp/src/lib.rs
+++ rollup/circuitscapacitychecker/libzkp/src/lib.rs
@@ -1,5 +1,34 @@
 #![feature(once_cell)]
 
+pub mod checker {
+    use crate::utils::{c_char_to_str, c_char_to_vec, vec_to_c_char};
+    use libc::c_char;
+    use std::cell::OnceCell;
+    use std::panic;
+    use std::ptr::null;
+    use types::eth::BlockTrace;
+    use zkevm::circuit::AGG_DEGREE;
+    use zkevm::{capacity_checker::CircuitCapacityChecker};
+
+    static mut CHECKER: OnceCell<CircuitCapacityChecker> = OnceCell::new();
+
+    /// # Safety
+    #[no_mangle]
+    pub unsafe extern "C" fn new_circuit_capacity_checker() {
+        // TODO: better logger
+        env_logger::init();
+
+        let c = CircuitCapacityChecker::new();
+        CHECKER.set(c).unwrap();
+    }
+
+    /// # Safety
+    #[no_mangle]
+    pub unsafe extern "C" fn reset_circuit_capacity_checker() {
+        CHECKER.get_mut().unwrap().reset()
+    }
+}
+
 pub(crate) mod utils {
     use std::ffi::{CStr, CString};
     use std::os::raw::c_char;

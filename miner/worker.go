e8cfd87f5d7f8de205c32022bcd66b59ae7a08ba
HAOYUatHZ <haoyu@protonmail.com>
2023-05-13 11:49:57 +0800

    filling

--- miner/worker.go
+++ miner/worker.go
@@ -785,22 +785,12 @@ func (w *worker) commitTransaction(tx *types.Transaction, coinbase common.Addres
 	tracer := w.chain.GetVMConfig().Tracer.(*vm.StructLogger)
 	tracer.Reset()
 
-	receipt, err := core.ApplyTransaction(w.chainConfig, w.chain, &coinbase, w.current.gasPool, w.current.state, w.current.header, tx, &w.current.header.GasUsed, *w.chain.GetVMConfig())
-	if err != nil {
-		w.current.state.RevertToSnapshot(snap)
-		return nil, err
-	}
-
 	// TODO:
 	traces := &types.BlockTrace{
-		// ChainID          uint64             `json:"chainID"`
 		ChainID: w.chainConfig.ChainID.Uint64(),
-
-		// Version          string             `json:"version"`
 		Version: params.Version,
 
 		// Coinbase         *AccountWrapper    `json:"coinbase"`
-		// Header           *Header            `json:"header"`
 		// Transactions     []*TransactionData `json:"transactions"`
 		// StorageTrace     *StorageTrace      `json:"storageTrace"`
 		// TxStorageTrace   []*StorageTrace    `json:"txStorageTrace,omitempty"`
@@ -808,6 +798,14 @@ func (w *worker) commitTransaction(tx *types.Transaction, coinbase common.Addres
 		// MPTWitness       *json.RawMessage   `json:"mptwitness,omitempty"`
 		// WithdrawTrieRoot common.Hash        `json:"withdraw_trie_root,omitempty"`
 	}
+
+	receipt, err := core.ApplyTransaction(w.chainConfig, w.chain, &coinbase, w.current.gasPool, w.current.state, w.current.header, tx, &w.current.header.GasUsed, *w.chain.GetVMConfig())
+	if err != nil {
+		w.current.state.RevertToSnapshot(snap)
+		return nil, err
+	}
+
+	traces.Header = w.current.header
 
 	if err := w.circuitsCapacityChecker.ApplyTransaction(traces); err != nil {
 		w.current.state.RevertToSnapshot(snap)

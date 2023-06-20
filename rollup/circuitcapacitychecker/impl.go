6685fcf304bc6f2081ac784886ab8e4d841f41bc
HAOYUatHZ <haoyu@protonmail.com>
2023-06-20 19:29:46 +0800

    up

--- rollup/circuitcapacitychecker/impl.go
+++ rollup/circuitcapacitychecker/impl.go
@@ -34,6 +34,8 @@ func (ccc *CircuitCapacityChecker) ApplyTransaction(traces *types.BlockTrace) er
 		return ErrUnknown
 	}
 
+	log.Error("trace", "string(tracesByt)", string(tracesByt))
+
 	tracesStr := C.CString(string(tracesByt))
 	defer func() {
 		C.free(unsafe.Pointer(tracesStr))

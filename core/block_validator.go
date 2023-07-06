eacd636f72dcbf0ddd83c4f80c2d39127a095971
HAOYUatHZ <haoyu@protonmail.com>
2023-07-06 13:20:05 +0800

    move 4

--- core/block_validator.go
+++ core/block_validator.go
@@ -21,14 +21,14 @@ import (
 	"fmt"
 	// "sync"
 
-	"github.com/scroll-tech/go-ethereum/common"
-	"github.com/scroll-tech/go-ethereum/common/hexutil"
+	// "github.com/scroll-tech/go-ethereum/common"
+	// "github.com/scroll-tech/go-ethereum/common/hexutil"
 	"github.com/scroll-tech/go-ethereum/consensus"
 	"github.com/scroll-tech/go-ethereum/core/rawdb"
 	"github.com/scroll-tech/go-ethereum/core/state"
 	"github.com/scroll-tech/go-ethereum/core/types"
-	"github.com/scroll-tech/go-ethereum/core/vm"
-	"github.com/scroll-tech/go-ethereum/log"
+	// "github.com/scroll-tech/go-ethereum/core/vm"
+	// "github.com/scroll-tech/go-ethereum/log"
 	"github.com/scroll-tech/go-ethereum/params"
 	"github.com/scroll-tech/go-ethereum/rollup/circuitcapacitychecker"
 	"github.com/scroll-tech/go-ethereum/trie"
@@ -226,51 +226,7 @@ func (v *BlockValidator) createTraceEnv(block *types.Block) (*TraceEnv, error) {
 		return nil, err
 	}
 
-	var coinbase common.Address
-	if v.config.Scroll.FeeVaultEnabled() {
-		coinbase = *v.config.Scroll.FeeVaultAddress
-	} else {
-		coinbase, err = v.engine.Author(block.Header())
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	env := &TraceEnv{
-		LogConfig: &vm.LogConfig{
-			EnableMemory:     false,
-			EnableReturnData: true,
-		},
-		Coinbase: coinbase,
-		Signer:   types.MakeSigner(v.config, block.Number()),
-		State:    statedb,
-		BlockCtx: NewEVMBlockContext(block.Header(), v.bc, nil),
-		StorageTrace: &types.StorageTrace{
-			RootBefore:    parent.Root(),
-			RootAfter:     block.Root(),
-			Proofs:        make(map[string][]hexutil.Bytes),
-			StorageProofs: make(map[string]map[string][]hexutil.Bytes),
-		},
-		ZkTrieTracer:     make(map[string]state.ZktrieProofTracer),
-		ExecutionResults: make([]*types.ExecutionResult, block.Transactions().Len()),
-		TxStorageTraces:  make([]*types.StorageTrace, block.Transactions().Len()),
-	}
-
-	key := coinbase.String()
-	if _, exist := env.Proofs[key]; !exist {
-		proof, err := env.State.GetProof(coinbase)
-		if err != nil {
-			log.Error("Proof for coinbase not available", "coinbase", coinbase, "error", err)
-			// but we still mark the proofs map with nil array
-		}
-		wrappedProof := make([]hexutil.Bytes, len(proof))
-		for i, bt := range proof {
-			wrappedProof[i] = bt
-		}
-		env.Proofs[key] = wrappedProof
-	}
-
-	return env, nil
+	return CreateTraceEnv(v.config, v.bc, v.engine, statedb, parent, block)
 }
 
 func (v *BlockValidator) validateCircuitRowUsage(block *types.Block) error {
--- core/trace.go
+++ core/trace.go
@@ -1,42 +1,93 @@
 package core
 
 import (
-    // "errors"
-    // "fmt"
-    "sync"
+	// "errors"
+	// "fmt"
+	"sync"
 
-    "github.com/scroll-tech/go-ethereum/common"
-    // "github.com/scroll-tech/go-ethereum/common/hexutil"
-    // "github.com/scroll-tech/go-ethereum/consensus"
-    // "github.com/scroll-tech/go-ethereum/core/rawdb"
-    "github.com/scroll-tech/go-ethereum/core/state"
-    "github.com/scroll-tech/go-ethereum/core/types"
-    "github.com/scroll-tech/go-ethereum/core/vm"
-    // "github.com/scroll-tech/go-ethereum/log"
-    // "github.com/scroll-tech/go-ethereum/params"
-    // "github.com/scroll-tech/go-ethereum/rollup/circuitcapacitychecker"
-    // "github.com/scroll-tech/go-ethereum/trie"
+	"github.com/scroll-tech/go-ethereum/common"
+	"github.com/scroll-tech/go-ethereum/common/hexutil"
+	"github.com/scroll-tech/go-ethereum/consensus"
+	// "github.com/scroll-tech/go-ethereum/core/rawdb"
+	"github.com/scroll-tech/go-ethereum/core/state"
+	"github.com/scroll-tech/go-ethereum/core/types"
+	"github.com/scroll-tech/go-ethereum/core/vm"
+	"github.com/scroll-tech/go-ethereum/log"
+	"github.com/scroll-tech/go-ethereum/params"
+	// "github.com/scroll-tech/go-ethereum/rollup/circuitcapacitychecker"
+	// "github.com/scroll-tech/go-ethereum/trie"
 )
 
 type TraceEnv struct {
-    LogConfig *vm.LogConfig
+	LogConfig   *vm.LogConfig
+	ChainConfig *params.ChainConfig
 
-    Coinbase common.Address
+	Coinbase common.Address
 
-    // rMu lock is used to protect txs executed in parallel.
-    Signer   types.Signer
-    State    *state.StateDB
-    BlockCtx vm.BlockContext
+	// rMu lock is used to protect txs executed in parallel.
+	Signer   types.Signer
+	State    *state.StateDB
+	BlockCtx vm.BlockContext
 
-    // pMu lock is used to protect Proofs' read and write mutual exclusion,
-    // since txs are executed in parallel, so this lock is required.
-    PMu sync.Mutex
-    // sMu is required because of txs are executed in parallel,
-    // this lock is used to protect StorageTrace's read and write mutual exclusion.
-    SMu sync.Mutex
-    *types.StorageTrace
-    TxStorageTraces []*types.StorageTrace
-    // zktrie tracer is used for zktrie storage to build additional deletion proof
-    ZkTrieTracer     map[string]state.ZktrieProofTracer
-    ExecutionResults []*types.ExecutionResult
+	// pMu lock is used to protect Proofs' read and write mutual exclusion,
+	// since txs are executed in parallel, so this lock is required.
+	PMu sync.Mutex
+	// sMu is required because of txs are executed in parallel,
+	// this lock is used to protect StorageTrace's read and write mutual exclusion.
+	SMu sync.Mutex
+	*types.StorageTrace
+	TxStorageTraces []*types.StorageTrace
+	// zktrie tracer is used for zktrie storage to build additional deletion proof
+	ZkTrieTracer     map[string]state.ZktrieProofTracer
+	ExecutionResults []*types.ExecutionResult
+}
+
+func CreateTraceEnv(chainConfig *params.ChainConfig, bc *BlockChain, engine consensus.Engine, statedb *state.StateDB, parent *types.Block, block *types.Block) (*TraceEnv, error) {
+	var coinbase common.Address
+	var err error
+	if chainConfig.Scroll.FeeVaultEnabled() {
+		coinbase = *chainConfig.Scroll.FeeVaultAddress
+	} else {
+		coinbase, err = engine.Author(block.Header())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	env := &TraceEnv{
+		LogConfig: &vm.LogConfig{
+			EnableMemory:     false,
+			EnableReturnData: true,
+		},
+		ChainConfig: chainConfig,
+		Coinbase:    coinbase,
+		Signer:      types.MakeSigner(chainConfig, block.Number()),
+		State:       statedb,
+		BlockCtx:    NewEVMBlockContext(block.Header(), bc, nil),
+		StorageTrace: &types.StorageTrace{
+			RootBefore:    parent.Root(),
+			RootAfter:     block.Root(),
+			Proofs:        make(map[string][]hexutil.Bytes),
+			StorageProofs: make(map[string]map[string][]hexutil.Bytes),
+		},
+		ZkTrieTracer:     make(map[string]state.ZktrieProofTracer),
+		ExecutionResults: make([]*types.ExecutionResult, block.Transactions().Len()),
+		TxStorageTraces:  make([]*types.StorageTrace, block.Transactions().Len()),
+	}
+
+	key := coinbase.String()
+	if _, exist := env.Proofs[key]; !exist {
+		proof, err := env.State.GetProof(coinbase)
+		if err != nil {
+			log.Error("Proof for coinbase not available", "coinbase", coinbase, "error", err)
+			// but we still mark the proofs map with nil array
+		}
+		wrappedProof := make([]hexutil.Bytes, len(proof))
+		for i, bt := range proof {
+			wrappedProof[i] = bt
+		}
+		env.Proofs[key] = wrappedProof
+	}
+
+	return env, nil
 }

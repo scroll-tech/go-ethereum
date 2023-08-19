0c477f5fae6ee557469d4f0b1192c277e2a2e9c6
Péter Garamvölgyi <peter@scroll.io>
2023-08-19 21:43:13 +0800

    fix worker snapshot

--- miner/worker.go
+++ miner/worker.go
@@ -873,11 +873,12 @@ func (w *worker) updateSnapshot() {
 }
 
 func (w *worker) commitTransaction(tx *types.Transaction, coinbase common.Address) ([]*types.Log, error) {
-	snap := w.current.state.Snapshot()
 	var accRows *types.RowConsumption
 
 	// do not do CCC checks on follower nodes
 	if w.isRunning() {
+		snap := w.current.state.Snapshot()
+
 		log.Trace(
 			"Worker apply ccc for tx",
 			"id", w.circuitCapacityChecker.ID,
@@ -917,7 +918,7 @@ func (w *worker) commitTransaction(tx *types.Transaction, coinbase common.Addres
 	}
 
 	// create new snapshot for `core.ApplyTransaction`
-	snap = w.current.state.Snapshot()
+	snap := w.current.state.Snapshot()
 
 	receipt, err := core.ApplyTransaction(w.chainConfig, w.chain, &coinbase, w.current.gasPool, w.current.state, w.current.header, tx, &w.current.header.GasUsed, *w.chain.GetVMConfig())
 	if err != nil {

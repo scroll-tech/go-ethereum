5aca85dea52f163da1f492886aa16e62e27e3856
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2023-08-30 14:09:22 +0800

    Update miner/worker.go
    Co-authored-by: Péter Garamvölgyi <peter@scroll.io>

--- miner/worker.go
+++ miner/worker.go
@@ -1123,7 +1123,7 @@ loop:
 			log.Trace("Unknown circuit capacity checker error for L2MessageTx", "tx", tx.Hash().String())
 			log.Info("Skipping L2 message", "tx", tx.Hash().String(), "block", w.current.header.Number, "reason", "unknown row consumption error")
 			// TODO: propagate more info about the error from CCC
-			rawdb.WriteSkippedTransaction(w.eth.ChainDb(), tx, "row consumption overflow", w.current.header.Number.Uint64(), nil)
+			rawdb.WriteSkippedTransaction(w.eth.ChainDb(), tx, "unknown circuit capacity checker error", w.current.header.Number.Uint64(), nil)
 
 			// Normally we would do `txs.Pop()` here.
 			// However, after `ErrUnknown`, ccc might remain in an

bc73411720600b45c088eb47e3be11027ab4f468
HAOYUatHZ <haoyu@protonmail.com>
2024-03-31 14:30:06 +0800

    fix

--- .github/scripts/bump_version_dot_go.mjs
+++ .github/scripts/bump_version_dot_go.mjs
@@ -1,37 +1,35 @@
-// import { URL } from "url";
-// import { readFileSync, writeFileSync } from "fs";
+import { URL } from "url";
+import { readFileSync, writeFileSync } from "fs";
 
-// const versionFilePath = new URL(
-//   "../../common/version/version.go",
-//   import.meta.url
-// ).pathname;
+const versionFilePath = new URL(
+  "../../params/version.go",
+  import.meta.url
+).pathname;
 
-// const versionFileContent = readFileSync(versionFilePath, { encoding: "utf-8" });
+const versionFileContent = readFileSync(versionFilePath, { encoding: "utf-8" });
 
-// const currentVersion = versionFileContent.match(
-//   /var tag = "(?<version>v(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+))"/
-// );
+const currentVersionPatch = versionFileContent.match(
+  /VersionPatch = (?<patch>\d+)/
+).groups.patch;
 
-// try {
-//   parseInt(currentVersion.groups.major);
-//   parseInt(currentVersion.groups.minor);
-//   parseInt(currentVersion.groups.patch);
-// } catch (err) {
-//   console.error(new Error("Failed to parse version in version.go file"));
-//   throw err;
-// }
+try {
+  parseInt(currentVersionPatch);
+} catch (err) {
+  console.error(new Error("Failed to parse version in version.go file"));
+  throw err;
+}
 
-// // prettier-ignore
-// const newVersion = `v${currentVersion.groups.major}.${currentVersion.groups.minor}.${parseInt(currentVersion.groups.patch) + 1}`;
+// prettier-ignore
+const newVersionPatch = `${parseInt(currentVersionPatch) + 1}`;
 
-// console.log(
-//   `Bump version from ${currentVersion.groups.version} to ${newVersion}`
-// );
+console.log(
+  `Bump version from ${currentVersionPatch} to ${newVersionPatch}`
+);
 
-// writeFileSync(
-//   versionFilePath,
-//   versionFileContent.replace(
-//     `var tag = "${currentVersion.groups.version}"`,
-//     `var tag = "${newVersion}"`
-//   )
-// );
+writeFileSync(
+  versionFilePath,
+  versionFileContent.replace(
+    `VersionPatch = ${currentVersionPatch}`,
+    `VersionPatch = ${newVersionPatch}`
+  )
+);

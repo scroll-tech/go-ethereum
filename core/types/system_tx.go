d630f16b78ae5c7e945c727a829a41a618a3fc6e
vyzo <vyzo@hackzen.org>
2024-04-25 15:50:33 +0300

    add Gas to SystemTx's

--- core/types/system_tx.go
+++ core/types/system_tx.go
@@ -10,6 +10,7 @@ import (
 
 type SystemTx struct {
 	ChainID *big.Int
+	Gas     uint64          // gas limit
 	To      *common.Address // system contract
 	Nonce   uint64          // nonce
 	Data    []byte          // calldata
@@ -21,6 +22,7 @@ func (tx *SystemTx) txType() byte { return SystemTxType }
 func (tx *SystemTx) copy() TxData {
 	cpy := &SystemTx{
 		ChainID: new(big.Int),
+		Gas:     tx.Gas,
 		Nonce:   tx.Nonce,
 		To:      copyAddressPtr(tx.To),
 		Data:    common.CopyBytes(tx.Data),
@@ -45,17 +47,20 @@ func (tx *SystemTx) copy() TxData {
 	return cpy
 }
 
-func (tx *SystemTx) chainID() *big.Int                      { return tx.ChainID }
-func (tx *SystemTx) accessList() AccessList                 { return nil }
-func (tx *SystemTx) data() []byte                           { return tx.Data }
-func (tx *SystemTx) gas() uint64                            { return 0 }
-func (tx *SystemTx) gasPrice() *big.Int                     { return new(big.Int) }
-func (tx *SystemTx) gasTipCap() *big.Int                    { return new(big.Int) }
-func (tx *SystemTx) gasFeeCap() *big.Int                    { return new(big.Int) }
-func (tx *SystemTx) value() *big.Int                        { return new(big.Int) }
-func (tx *SystemTx) nonce() uint64                          { return tx.Nonce }
-func (tx *SystemTx) to() *common.Address                    { return tx.To }
-func (tx *SystemTx) rawSignatureValues() (v, r, s *big.Int) { return tx.V, tx.R, tx.S }
+func (tx *SystemTx) chainID() *big.Int      { return tx.ChainID }
+func (tx *SystemTx) accessList() AccessList { return nil }
+func (tx *SystemTx) data() []byte           { return tx.Data }
+func (tx *SystemTx) gas() uint64            { return tx.Gas }
+func (tx *SystemTx) gasPrice() *big.Int     { return new(big.Int) }
+func (tx *SystemTx) gasTipCap() *big.Int    { return new(big.Int) }
+func (tx *SystemTx) gasFeeCap() *big.Int    { return new(big.Int) }
+func (tx *SystemTx) value() *big.Int        { return new(big.Int) }
+func (tx *SystemTx) nonce() uint64          { return tx.Nonce }
+func (tx *SystemTx) to() *common.Address    { return tx.To }
+
+func (tx *SystemTx) rawSignatureValues() (v, r, s *big.Int) {
+	return tx.V, tx.R, tx.S
+}
 
 func (tx *SystemTx) setSignatureValues(chainID, v, r, s *big.Int) {
 	tx.V, tx.R, tx.S = v, r, s

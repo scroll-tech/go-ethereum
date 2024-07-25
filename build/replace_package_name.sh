91ff49baee2fb12c0e467661c263f9773753b444
HAOYUatHZ <haoyu@protonmail.com>
2024-07-25 08:25:59 +0800

    clean up

--- build/replace_package_name.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-find . -name "*.go" -type f -exec sed -i '' 's|"github.com/ethereum/go-ethereum|"github.com/scroll-tech/go-ethereum|g' {} +
-find . -name "*.txt" -type f -exec sed -i '' 's|"github.com/ethereum/go-ethereum|"github.com/scroll-tech/go-ethereum|g' {} +
-find . -name "go.mod" -type f -exec sed -i '' 's|github.com/ethereum/go-ethereum|github.com/scroll-tech/go-ethereum|g' {} +
-

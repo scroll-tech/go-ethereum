5243d59d18df29ec64fcdb87fd6c8747206c9006
jonastheis <4181434+jonastheis@users.noreply.github.com>
2024-07-26 15:18:46 +0800

    refactor: introduce partial header and partial block for data from DA before execution

--- rollup/da_syncer/da_syncer.go
+++ rollup/da_syncer/da_syncer.go
@@ -2,12 +2,10 @@ package da_syncer
 
 import (
 	"fmt"
-	"math/big"
 
-	"github.com/scroll-tech/go-ethereum/common"
 	"github.com/scroll-tech/go-ethereum/core"
-	"github.com/scroll-tech/go-ethereum/core/types"
 	"github.com/scroll-tech/go-ethereum/log"
+	"github.com/scroll-tech/go-ethereum/rollup/da_syncer/da"
 )
 
 type DASyncer struct {
@@ -20,23 +18,19 @@ func NewDASyncer(blockchain *core.BlockChain) *DASyncer {
 	}
 }
 
-func (s *DASyncer) SyncOneBlock(block *types.Block) error {
+func (s *DASyncer) SyncOneBlock(block *da.PartialBlock) error {
 	parentBlock := s.blockchain.CurrentBlock()
-	if big.NewInt(0).Add(parentBlock.Number(), common.Big1).Cmp(block.Number()) != 0 {
-		return fmt.Errorf("not consecutive block, number: %d", block.Number())
+	if parentBlock.NumberU64()+1 != block.PartialHeader.Number {
+		return fmt.Errorf("not consecutive block, number: %d, chain height: %d", block.PartialHeader.Number, parentBlock.NumberU64())
 	}
 
-	header := block.Header()
-	header.Difficulty = common.Big1
-	header.BaseFee = nil // TODO: after Curie we need to fill this correctly
-	header.ParentHash = parentBlock.Hash()
-
-	if _, err := s.blockchain.BuildAndWriteBlock(parentBlock, header, block.Transactions()); err != nil {
-		return fmt.Errorf("failed building and writing block, number: %d, error: %v", block.Number(), err)
+	if _, err := s.blockchain.BuildAndWriteBlock(parentBlock, block.PartialHeader.ToHeader(), block.Transactions); err != nil {
+		return fmt.Errorf("failed building and writing block, number: %d, error: %v", block.PartialHeader.Number, err)
 	}
 
 	if s.blockchain.CurrentBlock().Header().Number.Uint64()%100 == 0 {
 		log.Info("inserted block", "blockhain height", s.blockchain.CurrentBlock().Header().Number, "block hash", s.blockchain.CurrentBlock().Header().Hash(), "root", s.blockchain.CurrentBlock().Header().Root)
 	}
+
 	return nil
 }

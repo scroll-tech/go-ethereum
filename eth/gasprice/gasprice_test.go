38f96138ab5b05e6173cbddfe828ca03c01ae237
HAOYUatHZ <haoyu@protonmail.com>
2024-08-01 21:39:37 +0800

    fix tests

--- eth/gasprice/gasprice_test.go
+++ eth/gasprice/gasprice_test.go
@@ -242,7 +242,7 @@ func TestSuggestTipCap(t *testing.T) {
 
 func TestSuggestTipCapCongestedThreshold(t *testing.T) {
 	expectedDefaultBasePricePreCurie := big.NewInt(2000)
-	expectedDefaultBasePricePostCurie := big.NewInt(1)
+	expectedDefaultBasePricePostCurie := big.NewInt(2)
 
 	config := Config{
 		Blocks:             3,

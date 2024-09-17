4680653f8805b6a68c46f8a76133bb3ae085c071
HAOYUatHZ <haoyu@protonmail.com>
2024-09-17 11:02:29 +1000

    fix: disable headCCCCheck for follower nodes

--- miner/scroll_worker.go
+++ miner/scroll_worker.go
@@ -287,9 +287,11 @@ func (w *worker) mainLoop() {
 
 		select {
 		case <-w.startCh:
-			if err := w.checkHeadRowConsumption(); err != nil {
-				log.Error("failed to start head checkers", "err", err)
-				return
+			if w.isRunning() {
+				if err := w.checkHeadRowConsumption(); err != nil {
+					log.Error("failed to start head checkers", "err", err)
+					return
+				}
 			}
 
 			_, err = w.tryCommitNewWork(time.Now(), w.chain.CurrentHeader().Hash(), nil)

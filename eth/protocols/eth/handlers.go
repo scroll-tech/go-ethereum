d59e79b16623469d0f5933994778d1ad78714f33
Alejandro Ranchal-Pedrosa <a.ranchalpedrosa@gmail.com>
2025-02-17 11:38:46 -0300

    Make new RLP optional fields always default for legacy/reth compatibility

--- eth/protocols/eth/handlers.go
+++ eth/protocols/eth/handlers.go
@@ -266,6 +266,11 @@ func handleNewBlock(backend Backend, msg Decoder, peer *Peer) error {
 	if err := msg.Decode(ann); err != nil {
 		return fmt.Errorf("%w: message %v: %v", errDecode, msg, err)
 	}
+
+	hCopy := ann.Block.Header()
+	hCopy.PrepareFromNetwork(backend.Chain().Config().IsEuclidV2(hCopy.Time))
+	ann.Block = ann.Block.CopyBlockDeepWithHeader(hCopy)
+
 	if err := ann.sanityCheck(); err != nil {
 		return err
 	}
@@ -294,6 +299,13 @@ func handleBlockHeaders66(backend Backend, msg Decoder, peer *Peer) error {
 	}
 	requestTracker.Fulfil(peer.id, peer.version, BlockHeadersMsg, res.RequestId)
 
+	headersCopy := make([]*types.Header, 0, len(res.BlockHeadersPacket))
+	for _, header := range res.BlockHeadersPacket {
+		hCopy := types.CopyHeader(header)
+		hCopy.PrepareFromNetwork(backend.Chain().Config().IsEuclidV2(header.Time))
+		headersCopy = append(headersCopy, hCopy)
+	}
+	res.BlockHeadersPacket = headersCopy
 	return backend.Handle(peer, &res.BlockHeadersPacket)
 }
 

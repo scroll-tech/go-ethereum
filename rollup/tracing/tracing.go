a83f43b3a49384b212aa8aa20d8aa6609a460c10
Ömer Faruk Irmak <omerfirmak@gmail.com>
2025-02-19 13:51:23 +0300

    fix: disallow tracing post-euclid blocks with scroll specific RPC methods

--- rollup/tracing/tracing.go
+++ rollup/tracing/tracing.go
@@ -182,6 +182,10 @@ func CreateTraceEnv(chainConfig *params.ChainConfig, chainContext core.ChainCont
 }
 
 func (env *TraceEnv) GetBlockTrace(block *types.Block) (*types.BlockTrace, error) {
+	if env.chainConfig.IsEuclid(block.Time()) {
+		return nil, errors.New("cannot trace post euclid blocks with scroll specific RPC methods")
+	}
+
 	// Execute all the transaction contained within the block concurrently
 	var (
 		txs   = block.Transactions()

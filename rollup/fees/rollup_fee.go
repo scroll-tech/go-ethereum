0139eec25d8742dd3490c0e1025eac5b4b98ea8e
colinlyguo <colinlyguo@scroll.io>
2025-02-25 23:01:19 +0800

    fix asUnsignedTx

--- rollup/fees/rollup_fee.go
+++ rollup/fees/rollup_fee.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"math/big"
 
+	"github.com/holiman/uint256"
 	"github.com/scroll-tech/go-ethereum/common"
 	"github.com/scroll-tech/go-ethereum/core/types"
 	"github.com/scroll-tech/go-ethereum/crypto"
@@ -35,6 +36,7 @@ type Message interface {
 	Data() []byte
 	AccessList() types.AccessList
 	IsL1MessageTx() bool
+	SetCodeAuthorizations() []types.SetCodeAuthorization
 }
 
 // StateDB represents the StateDB interface
@@ -92,7 +94,11 @@ func asUnsignedTx(msg Message, baseFee, chainID *big.Int) *types.Transaction {
 		return asUnsignedAccessListTx(msg, chainID)
 	}
 
-	return asUnsignedDynamicTx(msg, chainID)
+	if msg.SetCodeAuthorizations() == nil {
+		return asUnsignedDynamicTx(msg, chainID)
+	}
+
+	return asUnsignedSetCodeTx(msg, chainID)
 }
 
 func asUnsignedLegacyTx(msg Message) *types.Transaction {
@@ -130,6 +136,21 @@ func asUnsignedDynamicTx(msg Message, chainID *big.Int) *types.Transaction {
 		Data:       msg.Data(),
 		AccessList: msg.AccessList(),
 		ChainID:    chainID,
+	})
+}
+
+func asUnsignedSetCodeTx(msg Message, chainID *big.Int) *types.Transaction {
+	return types.NewTx(&types.SetCodeTx{
+		Nonce:      msg.Nonce(),
+		To:         *msg.To(),
+		Value:      uint256.MustFromBig(msg.Value()),
+		Gas:        msg.Gas(),
+		GasFeeCap:  uint256.MustFromBig(msg.GasFeeCap()),
+		GasTipCap:  uint256.MustFromBig(msg.GasTipCap()),
+		Data:       msg.Data(),
+		AccessList: msg.AccessList(),
+		AuthList:   msg.SetCodeAuthorizations(),
+		ChainID:    uint256.MustFromBig(chainID),
 	})
 }
 

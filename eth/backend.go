07559cb9f8cd090ff840f3d8d7454e8c4006ef36
Alejandro Ranchal-Pedrosa <a.ranchalpedrosa@gmail.com>
2025-02-25 23:52:28 -0300

    Fix indentation issue

--- eth/backend.go
+++ eth/backend.go
@@ -281,7 +281,7 @@ func New(stack *node.Node, config *ethconfig.Config, l1Client l1.Client) (*Ether
 
 	config.Miner.SigningDisabled = config.DA.ProduceBlocks
 	eth.miner = miner.New(eth, &config.Miner, eth.blockchain.Config(), eth.EventMux(), eth.engine, eth.isLocalBlock, config.EnableDASyncing && !config.DA.ProduceBlocks)
-  // Some of the extraData is used with Clique consensus (before EuclidV2). After EuclidV2 we use SystemContract consensus where this is overridden when creating a block.
+	// Some of the extraData is used with Clique consensus (before EuclidV2). After EuclidV2 we use SystemContract consensus where this is overridden when creating a block.
 	eth.miner.SetExtra(makeExtraData(config.Miner.ExtraData))
 
 	eth.APIBackend = &EthAPIBackend{stack.Config().ExtRPCEnabled(), stack.Config().AllowUnprotectedTxs, eth, nil}

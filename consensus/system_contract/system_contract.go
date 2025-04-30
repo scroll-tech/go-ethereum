85ef93355a6759616b83e8fde527b2fd4cb69903
Morty <yiweichi1@gmail.com>
2025-04-30 21:16:56 +0800

    debug system config consensus

--- consensus/system_contract/system_contract.go
+++ consensus/system_contract/system_contract.go
@@ -95,8 +95,8 @@ func (s *SystemContract) fetchAddressFromL1() error {
 	}
 	// bAddress := common.HexToAddress("0x756ea06bdee36de11f22dcca45a31d8a178ef3c6")
 	// bAddress := common.HexToAddress("0xD9E635beA2Ed2813eD1c550429FB965dEE58109B")
-	bAddress := common.Address{}
-	// bAddress := common.HexToAddress("0xfe3Ba6Fac4Fdd5C975A61089AF2a3508a86D6a8B")
+	// bAddress := common.Address{}
+	bAddress := common.HexToAddress("0xfe3Ba6Fac4Fdd5C975A61089AF2a3508a86D6a8B")
 	s.lock.Lock()
 	defer s.lock.Unlock()
 

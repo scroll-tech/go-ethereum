a36f63c01e83114b0682741ac7fb9a3141de5101
colinlyguo <colinlyguo@scroll.io>
2025-06-25 06:08:36 +0800

    tweaks

--- rollup/fees/rollup_fee.go
+++ rollup/fees/rollup_fee.go
@@ -169,7 +169,7 @@ func readGPOStorageSlots(addr common.Address, state StateDB) gpoState {
 // compression_ratio(tx) = size(tx) * PRECISION / size(zstd(tx))
 func calculateCompressionRatio(data []byte) *big.Int {
 	// FIXME: This is a placeholder for the actual compression ratio calculation in another PR.
-	return big.NewInt(1_000_000_000) // 1 * PRECISION
+	return rcfg.Precision
 }
 
 // calculatePenalty computes the penalty multiplier based on compression ratio
@@ -177,10 +177,10 @@ func calculateCompressionRatio(data []byte) *big.Int {
 func calculatePenalty(compressionRatio, penaltyThreshold, penaltyFactor *big.Int) *big.Int {
 	if compressionRatio.Cmp(penaltyThreshold) >= 0 {
 		// No penalty
-		return new(big.Int).Set(rcfg.Precision)
+		return rcfg.Precision
 	}
 	// Apply penalty
-	return new(big.Int).Set(penaltyFactor)
+	return penaltyFactor
 }
 
 // calculateEncodedL1DataFee computes the L1 fee for an RLP-encoded tx
